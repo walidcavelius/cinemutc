e7e93dfb41bb91099b792f6817b6c2677106a05d
w.cavelius <w.cavelius@criteo.com>
2024-10-31 18:29:55 +0100

    Add release year and directors on film page

--- app/views/components/navbar.php
+++ app/views/components/navbar.php
@@ -1,9 +1,9 @@
 <header class="border-b-2 border-gray-200 flex justify-between p-5 px-8 items-center">
-	<a href="/cinemutc/public"><img src="../../../cinemutc/public/assets/logo.png" width="200" /></a>
-	<ul class="flex flex-row justify-between">
-		<li><a href="/cinemutc/public/programmation" class="mr-6">programmation</a></li>
-		<li><a href="/cinemutc/public/archive" class="mr-6">archive</a></li>
-		<li><a href="/cinemutc/public/infos" class="mr-6">à propos</a></li>
-		<li><a href="#">connexion</a></li>
-	</ul>
+  <a href="/cinemutc/public"><img src="../../../cinemutc/public/assets/logo.png" width="200" /></a>
+  <ul class="flex flex-row justify-between">
+    <li><a href="/cinemutc/public/programmation" class="mr-6">Programmation</a></li>
+    <li><a href="/cinemutc/public/archive" class="mr-6">Archive</a></li>
+    <li><a href="/cinemutc/public/infos" class="mr-6">À propos</a></li>
+    <li><a href="#">Connexion</a></li>
+  </ul>
 </header>
--- app/views/film.php
+++ app/views/film.php
@@ -4,24 +4,28 @@ ob_start();
 ?>
 
 <div class="w-full h-full">
-    <div class="max-w-5xl relative m-auto h-full">
-            <div class="-z-50 relative m-auto bg-center bg-cover h-96"
-                style="background-image:url(https://image.tmdb.org/t/p/<?php echo $film['backdrop_path']; ?>); 
+  <div class="max-w-5xl relative m-auto h-full">
+    <div class="-z-50 relative m-auto bg-center bg-cover h-96"
+      style="background-image:url(https://image.tmdb.org/t/p/<?php echo $film['backdrop_path']; ?>); 
                        box-shadow: inset 5px 5px 100px 70px rgb(243 244 246), inset -5px -5px 100px 70px rgb(243 244 246);">
-            </div>
-            <div class="mt-[-5%] flex justify-start gap-3">
-                <a href="" class="text-center flex w-1/4">
-                    <img class="border-2 rounded-md border-gray-400 hover:border-gray-700"
-                         src="https://image.tmdb.org/t/p/w500<?php echo $film['poster_path']; ?>"
-                         alt="<?php echo $film['titre']; ?>" />
-                </a>
-                <div class="ml-10 mt-4 flex flex-col w-3/4">
-                    <h1 class="text-3xl"><?php echo $film['titre']; ?></h1>
-                    <p class="text-md my-4"><?php echo $film['overview']; ?></p>
-                </div>
-                <div class=""></div>
-            </div>
     </div>
+    <div class="mt-[-5%] flex justify-start gap-3">
+      <a href="" class="text-center flex w-1/4">
+        <img class="border-2 rounded-md border-gray-400 hover:border-gray-700"
+          src="https://image.tmdb.org/t/p/w500<?php echo $film['poster_path']; ?>"
+          alt="<?php echo $film['titre']; ?>" />
+      </a>
+      <div class="ml-10 mt-4 flex flex-col w-3/4">
+        <div class="flex items-end space-x-4">
+          <h1 class="text-3xl"><?php echo $film['titre']; ?></h1>
+          <h2 class="text-lg"><?php echo $film['release_date']; ?></h2>
+          <h2 class="text-lg">Réalisé par <?php echo $film['directors']; ?></h2>
+        </div>
+        <p class="text-md my-4"><?php echo $film['overview']; ?></p>
+      </div>
+      <div class=""></div>
+    </div>
+  </div>
 </div>
 
 
